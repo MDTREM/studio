89b875ac2b432459f948ce2e408363353d6059d6
Matheus Diniz <m77heus@gmail.com>
2025-11-25 13:36:07 +0000

    remova as setas do banner, coloque apenas bolinhas de identificação de p

--- src/components/sections/home/Hero.tsx
+++ src/components/sections/home/Hero.tsx
@@ -3,15 +3,32 @@ import {
   Carousel,
   CarouselContent,
   CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
+  type CarouselApi,
 } from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
+import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
 
 export default function Hero() {
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+
   return (
     <section className="relative w-full h-[60vh] md:h-[80vh]">
       <Carousel
+        setApi={setApi}
         className="w-full h-full"
         plugins={[
           Autoplay({
@@ -31,9 +48,15 @@ export default function Hero() {
             </CarouselItem>
           ))}
         </CarouselContent>
-        <CarouselPrevious className="absolute left-4 top-1/2 -translate-y-1/2 text-white" />
-        <CarouselNext className="absolute right-4 top-1/2 -translate-y-1/2 text-white" />
       </Carousel>
+      <div className="absolute bottom-4 left-1/2 -translate-x-1/2 flex items-center gap-2">
+        {api?.scrollSnapList().map((_, index) => (
+            <button key={index} onClick={() => api.scrollTo(index)} className={cn(
+                "w-2 h-2 rounded-full",
+                current === index + 1 ? "bg-white" : "bg-white/50"
+            )}></button>
+        ))}
+      </div>
     </section>
   );
 }
