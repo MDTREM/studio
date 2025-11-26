862ba7da022e4a86dafbe701dce563d047042c9e
Matheus Diniz <m77heus@gmail.com>
2025-11-26 17:44:31 +0000

    Application error: a server-side exception has occurred while loading 60

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -1,10 +1,22 @@
-import { FirebaseApp } from "firebase/app";
+import { FirebaseApp, getApp, getApps, initializeApp } from "firebase/app";
 import { Auth, getAuth } from "firebase/auth";
 import { Firestore, getFirestore } from "firebase/firestore";
+import { firebaseConfig } from "./config";
 
 // This file does not have 'use client' and can be used in Server Components.
 
-export function getSdks(firebaseApp: FirebaseApp): { firebaseApp: FirebaseApp; auth: Auth; firestore: Firestore; } {
+// Helper function to initialize Firebase if it hasn't been already.
+// This is safe to call from both server and client.
+const initializeFirebaseApp = (): FirebaseApp => {
+  if (!getApps().length) {
+    // When on the server, we must use the config object.
+    return initializeApp(firebaseConfig);
+  }
+  return getApp();
+};
+
+export function getSdks(): { firebaseApp: FirebaseApp; auth: Auth; firestore: Firestore; } {
+  const firebaseApp = initializeFirebaseApp();
   const auth = getAuth(firebaseApp);
   auth.languageCode = 'pt';
   return {
