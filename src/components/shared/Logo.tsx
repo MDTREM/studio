c7cc7d798ff0e55f7fc7162dbf4dc8c89b993d88
Matheus Diniz <m77heus@gmail.com>
2025-12-01 15:12:41 +0000

    não ajustou

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 export default function Logo() {
   return (
-    <div className="flex items-center gap-2 text-lg font-bold">
+    <div className="flex items-center gap-2 text-lg font-bold h-10">
       <Image
         src="https://i.imgur.com/nDzOzwX.png"
         alt="Ouro Gráfica Logo"
         width={180}
         height={40}
-        className="w-auto h-10" // Controla a altura e deixa a largura automática
+        className="w-auto h-full"
         priority
       />
     </div>
