3a2043cf92cf3e757b86c730efe00b05b200a7ce
Matheus Diniz <m77heus@gmail.com>
2025-12-09 15:27:42 +0000

    e o mais vendidos aparecerá primeiro embaixo dos 3 banners e o produtos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,9 +12,9 @@ export default function Home() {
       <Hero />
       <InfoCarousel />
       <PromoBanners />
-      <FeaturedProducts />
       <BestsellersSection />
       <CategoriesSection />
+      <FeaturedProducts />
       <PrinterBanners />
     </>
   );
