d84a8923191cdd2a558a789d25cc06169373bd4b
Matheus Diniz <m77heus@gmail.com>
2025-12-09 18:47:01 +0000

    pro banner principal adicione: desktop: https://i.imgur.com/T0wPi26.png;

--- src/components/sections/home/Hero.tsx
+++ src/components/sections/home/Hero.tsx
@@ -1,10 +1,29 @@
 "use client";
 
+import Image from "next/image";
+
 export default function Hero() {
   return (
-    <section className="relative w-full h-[60vh]">
-      <div className="relative w-full h-full bg-primary flex items-center justify-center">
-        <h1 className="text-4xl font-bold text-primary-foreground">Banner</h1>
+    <section className="relative w-full h-[60vh] bg-black">
+      {/* Imagem para Mobile */}
+      <div className="md:hidden relative w-full h-full">
+        <Image
+          src="https://i.imgur.com/DHLrPss.png"
+          alt="Banner para mobile"
+          fill
+          className="object-cover"
+          priority
+        />
+      </div>
+      {/* Imagem para Desktop */}
+      <div className="hidden md:block relative w-full h-full">
+        <Image
+          src="https://i.imgur.com/T0wPi26.png"
+          alt="Banner para desktop"
+          fill
+          className="object-cover"
+          priority
+        />
       </div>
     </section>
   );
