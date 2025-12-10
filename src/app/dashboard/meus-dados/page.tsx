2858c8b09a97e69a0e516888e7a48477b60c6b93
Matheus Diniz <m77heus@gmail.com>
2025-12-10 13:25:36 +0000

    Try fixing this error: `Unhandled Runtime Error: ReferenceError: useMemo

--- src/app/dashboard/meus-dados/page.tsx
+++ src/app/dashboard/meus-dados/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { useDoc, useFirebase, useUser } from "@/firebase";
+import { useDoc, useFirebase, useUser, useMemoFirebase } from "@/firebase";
 import { User } from "@/lib/definitions";
 import { doc, updateDoc } from "firebase/firestore";
 import { useForm } from "react-hook-form";
